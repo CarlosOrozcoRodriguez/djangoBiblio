042956e499851df1f97cda07669f56f9a88404bd
Carlos Orozco <ceor.orozco@gmail.com>
2024-01-12 17:08:39 +0100

    Actualizado modelo
    sobraba un campo en el modelo de Book

--- loans/models.py
+++ loans/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    user_id = models.IntegerField()
     title = models.CharField(max_length = 255)
     author = models.CharField(max_length = 255)
     genre = models.CharField(max_length = 255)
